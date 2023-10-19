218f6f8a7face2a0772736cccd18038bf6dc508c
VictorPithan <victorpithan@gmail.com>
2023-10-19 15:00:47 -0300

    feat: add annotation RestController

--- src/main/java/br/com/victorpithan/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/br/com/victorpithan/parkingcontrol/ParkingControlApplication.java
@@ -2,8 +2,10 @@ package br.com.victorpithan.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
